e298cf7542284d7bfde5fa873881d5e52c2299b9
thisisolya <olyapinch@gmail.com>
2022-03-25 10:18:54 +0300

    add eslint, remove notistack, amend gitignore

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from "@emotion/react";
 import { Provider } from "react-redux";
-import { SnackbarProvider } from "notistack";
 import { BrowserRouter } from "react-router-dom";
 
 import theme from "./theme";
@@ -13,9 +12,7 @@ const App = () => {
     <BrowserRouter>
       <Provider store={store}>
         <ThemeProvider theme={theme}>
-          <SnackbarProvider maxSnack={3} autoHideDuration={1500}>
-            <MainPage />
-          </SnackbarProvider>
+          <MainPage />
         </ThemeProvider>
       </Provider>
     </BrowserRouter>
